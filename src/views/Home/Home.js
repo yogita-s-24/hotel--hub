9fea9ea8b71dd9818e7dbe8a95e841c178e3f728
sagar-wandile <wandilesagar2926@gmail.com>
2023-09-15 16:40:21 +0530

    design pages

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar';
-
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <h1>home</h1>
+  
     </>
   )
 }
